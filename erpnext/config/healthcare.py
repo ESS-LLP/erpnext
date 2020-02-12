c0b928a6d5122d4697f6b5bd6b10e733d280169e
Jamsheer <jamsheer@earthianslive.com>
2020-02-12 13:17:41 +0530

    fix: Healthcare - changed menu config

--- erpnext/config/healthcare.py
+++ erpnext/config/healthcare.py
@@ -5,8 +5,22 @@ def get_data():
 
 	return [
 		{
-			"label": _("Consultation"),
-			"icon": "icon-star",
+			"label": _("Patient"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Patient",
+					"label": _("Patient")
+				},
+				{
+					"type": "page",
+					"name": "patient_history",
+					"label": _("Patient History"),
+				}
+			]
+		},
+		{
+			"label": _("Outpatient"),
 			"items": [
 				{
 					"type": "doctype",
@@ -15,19 +29,115 @@ def get_data():
 				},
 				{
 					"type": "doctype",
-					"name": "Patient Encounter",
-					"label": _("Patient Encounter"),
-				},
-				{
-					"type": "doctype",
 					"name": "Vital Signs",
 					"label": _("Vital Signs"),
 					"description": _("Record Patient Vitals"),
 				},
 				{
-					"type": "page",
-					"name": "patient_history",
-					"label": _("Patient History"),
+					"type": "doctype",
+					"name": "Patient Encounter",
+					"label": _("Patient Encounter"),
+				}
+			]
+		},
+		{
+			"label": _("Inpatient"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Inpatient Record",
+					"label": _("Inpatient Record")
+				},
+				{
+					"type": "doctype",
+					"name": "Healthcare Nursing Task",
+					"label": _("Healthcare Nursing Task"),
+				}
+			]
+		},
+		{
+			"label": _("Laboratory"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Lab Test",
+					"label": _("Lab Order")
+				},
+				{
+					"type": "doctype",
+					"name": "Sample Collection",
+					"label": _("Specimen Collection")
+				},
+				{
+					"type": "report",
+					"name": "Lab Test Report",
+					"is_query_report": True,
+					"label": _("Lab Test Report")
+				}
+			]
+		},
+		{
+			"label": _("Radiology"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Radiology Examination",
+					"label": _("Radiology Examination")
+				}
+			]
+		},
+		{
+			"label": _("Operation Theature"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Clinical Procedure",
+					"label": _("Procedure")
+				}
+			]
+		},
+		{
+			"label": _("Billing and Insurance"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Insurance Assignment",
+					"label": _("Insurance Assignment"),
+				},
+				{
+					"type": "doctype",
+					"name": "Insurance Claim",
+					"label": _("Insurance Claim"),
+				},
+				{
+					"type": "doctype",
+					"name": "Insurance Claim Submission",
+					"label": _("Insurance Claim Submission"),
+				},
+				{
+					"type": "doctype",
+					"name": "Contract Type",
+					"label": _("Contract Type"),
+				},
+				{
+					"type": "doctype",
+					"name": "Insurance Contract",
+					"label": _("Insurance Contract"),
+				},
+				{
+					"type": "doctype",
+					"name": "Practitioner Service Profile",
+					"label": _("Practitioner Service Profile"),
+				},
+				{
+					"type": "doctype",
+					"name": "Healthcare Service Profile Assignment",
+					"label": _("Healthcare Service Profile Assignment"),
+				},
+				{
+					"type": "doctype",
+					"name": "Healthcare Service Revenue Allocation",
+					"label": _("Healthcare Service Revenue Allocation"),
 				},
 				{
 					"type": "page",
@@ -42,18 +152,32 @@ def get_data():
 				},
 				{
 					"type": "doctype",
-					"name": "Clinical Procedure",
-					"label": _("Clinical Procedure"),
+					"name": "Insurance Type",
+					"label": _("Insurance Type"),
 				},
 				{
 					"type": "doctype",
-					"name": "Radiology Examination",
-					"label": _("Radiology Examination"),
+					"name": "Insurance Company",
+					"label": _("Insurance Company"),
 				},
 				{
 					"type": "doctype",
-					"name": "Inpatient Record",
-					"label": _("Inpatient Record"),
+					"name": "Insurance plan",
+					"label": _("Insurance plan"),
+				}
+			]
+		},
+		{
+			"label": _("Pharmacy"),
+			"items": []
+		},
+		{
+			"label": _("Practitioner Management"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Healthcare Practitioner",
+					"label": _("Healthcare Practitioner")
 				},
 				{
 					"type": "doctype",
@@ -62,66 +186,8 @@ def get_data():
 				},
 				{
 					"type": "doctype",
-					"name": "Healthcare Nursing Task",
-					"label": _("Healthcare Nursing Task"),
-				}
-			]
-		},
-		{
-			"label": _("Laboratory"),
-			"icon": "icon-list",
-			"items": [
-				{
-					"type": "doctype",
-					"name": "Lab Test",
-					"label": _("Lab Test"),
-				},
-				{
-					"type": "doctype",
-					"name": "Sample Collection",
-					"label": _("Sample Collection"),
-				},
-				{
-					"type": "report",
-					"name": "Lab Test Report",
-					"is_query_report": True,
-					"label": _("Lab Test Report"),
-				}
-			]
-		},
-		{
-			"label": _("Masters"),
-			"icon": "icon-list",
-			"items": [
-				{
-					"type": "doctype",
-					"name": "Patient",
-					"label": _("Patient"),
-				},
-				{
-					"type": "doctype",
-					"name": "Healthcare Practitioner",
-					"label": _("Healthcare Practitioner"),
-				},
-				{
-					"type": "doctype",
 					"name": "Practitioner Schedule",
 					"label": _("Practitioner Schedule"),
-				},
-				{
-					"type": "doctype",
-					"name": "Medical Code Standard",
-					"label": _("Medical Code Standard"),
-				},
-				{
-					"type": "doctype",
-					"name": "Medical Code",
-					"label": _("Medical Code"),
-				},
-				{
-					"type": "doctype",
-					"name": "Healthcare Service Unit",
-					"label": _("Healthcare Service Unit")
 				},
 				{
 					"type": "doctype",
@@ -131,8 +197,38 @@ def get_data():
 			]
 		},
 		{
+			"label": _("Facility Management"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Healthcare Service Unit",
+					"label": _("Healthcare Service Unit"),
+					"route": "Tree/Healthcare Service Unit"
+				},
+				{
+					"type": "doctype",
+					"name": "Healthcare Service Unit Type",
+					"label": _("Healthcare Service Unit Type")
+				}
+			]
+		},
+		{
+			"label": _("Medical Code"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Medical Code Standard",
+					"label": _("Medical Code Standard"),
+				},
+				{
+					"type": "doctype",
+					"name": "Medical Code",
+					"label": _("Medical Code"),
+				}
+			]
+		},
+		{
 			"label": _("Setup"),
-			"icon": "icon-cog",
 			"items": [
 				{
 					"type": "doctype",
@@ -149,6 +245,46 @@ def get_data():
 					"name": "Appointment Type",
 					"label": _("Appointment Type"),
 				},
+				{
+					"type": "doctype",
+					"name": "Lab Test Sample",
+					"label": _("Lab Test Sample"),
+				},
+				{
+					"type": "doctype",
+					"name": "Lab Test UOM",
+					"label": _("Lab Test UOM")
+				},
+				{
+					"type": "doctype",
+					"name": "Lab Test Template",
+					"label": _("Lab Test Template")
+				},
+				{
+					"type": "doctype",
+					"name": "Clinical Procedure Template",
+					"label": _("Clinical Procedure Template"),
+				},
+				{
+					"type": "doctype",
+					"name": "Clinical Procedure Check List Template",
+					"label": _("Clinical Procedure Check List Template"),
+				},
+				{
+					"type": "doctype",
+					"name": "Radiology Procedure",
+					"label": _("Radiology Procedure")
+				},
+				{
+					"type": "doctype",
+					"name": "Modality Type",
+					"label": _("Modality Type")
+				}
+			]
+		},
+		{
+			"label": _("Additional Setup"),
+			"items": [
 				{
 					"type": "doctype",
 					"name": "Prescription Dosage",
@@ -171,16 +307,6 @@ def get_data():
 				},
 				{
 					"type": "doctype",
-					"name": "Lab Test Sample",
-					"label": _("Lab Test Sample"),
-				},
-				{
-					"type": "doctype",
-					"name": "Lab Test UOM",
-					"label": _("Lab Test UOM")
-				},
-				{
-					"type": "doctype",
 					"name": "Antibiotic",
 					"label": _("Antibiotic")
 				},
@@ -193,97 +319,6 @@ def get_data():
 					"type": "doctype",
 					"name": "Organism",
 					"label": _("Organism")
-				},
-				{
-					"type": "doctype",
-					"name": "Lab Test Template",
-					"label": _("Lab Test Template")
-				},
-				{
-					"type": "doctype",
-					"name": "Clinical Procedure Template",
-					"label": _("Clinical Procedure Template"),
-				},
-				{
-					"type": "doctype",
-					"name": "Healthcare Service Unit Type",
-					"label": _("Healthcare Service Unit Type")
-				},
-				{
-					"type": "doctype",
-					"name": "Clinical Procedure Check List Template",
-					"label": _("Clinical Procedure Check List Template"),
-				},
-				{
-					"type": "doctype",
-					"name": "Modality Type",
-					"label": _("Modality Type")
-				},
-				{
-					"type": "doctype",
-					"name": "Radiology Procedure",
-					"label": _("Radiology Procedure")
-				}
-			]
-		},
-		{
-			"label": _("Patient Insurance"),
-			"icon": "icon-list",
-			"items": [
-				{
-					"type": "doctype",
-					"name": "Insurance Assignment",
-					"label": _("Insurance Assignment"),
-				},
-				{
-					"type": "doctype",
-					"name": "Insurance Claim",
-					"label": _("Insurance Claim"),
-				},
-				{
-					"type": "doctype",
-					"name": "Insurance Claim Submission",
-					"label": _("Insurance Claim Submission"),
-				},
-				{
-					"type": "doctype",
-					"name": "Insurance Type",
-					"label": _("Insurance Type"),
-				},
-				{
-					"type": "doctype",
-					"name": "Insurance Company",
-					"label": _("Insurance Company"),
-				},
-				{
-					"type": "doctype",
-					"name": "Insurance plan",
-					"label": _("Insurance plan"),
-				},
-				{
-					"type": "doctype",
-					"name": "Contract Type",
-					"label": _("Contract Type"),
-				},
-				{
-					"type": "doctype",
-					"name": "Insurance Contract",
-					"label": _("Insurance Contract"),
-				},
-				{
-					"type": "doctype",
-					"name": "Healthcare Service Profile Assignment",
-					"label": _("Healthcare Service Profile Assignment"),
-				},
-				{
-					"type": "doctype",
-					"name": "Practitioner Service Profile",
-					"label": _("Practitioner Service Profile"),
-				},
-				{
-					"type": "doctype",
-					"name": "Healthcare Service Revenue Allocation",
-					"label": _("Healthcare Service Revenue Allocation"),
 				}
 			]
 		}
