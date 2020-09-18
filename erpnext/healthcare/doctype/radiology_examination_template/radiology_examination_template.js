f054080802befb3fecc68c0908831b7ce7d8a9c6
akash <akashkrishna619@gmail.com>
2020-09-18 16:10:59 +0530

    feat: Radiology - doctypes

---
+++ erpnext/healthcare/doctype/radiology_examination_template/radiology_examination_template.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Radiology Examination Template', {
+});
