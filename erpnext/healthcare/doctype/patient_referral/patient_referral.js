dc8fd961d145c33365f3b3eb5ea1ea2115a7a918
akash <akashkrishna619@gmail.com>
2020-09-27 19:22:21 +0530

    fix: Patient Referral - appointment booking

--- erpnext/healthcare/doctype/patient_referral/patient_referral.js
+++ erpnext/healthcare/doctype/patient_referral/patient_referral.js
@@ -2,7 +2,19 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Patient Referral', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (!frm.doc.__islocal) {
+			if (frm.doc.status == 'Pending' && frm.doc.docstatus == 1) {
+				frm.add_custom_button(__('Book Appointment'), function() {
+					frappe.route_options = {
+						'patient': frm.doc.patient,
+						'practitioner': frm.doc.referred_to_practitioner,
+						'patient_referral': frm.doc.name,
+						'company': frm.doc.company,
+					};
+					frappe.new_doc('Patient Appointment');
+				});
+			}
+		}
+	}
 });
