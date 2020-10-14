a85e0fa6daa4eb007725debbf3d57c2cf936be49
akash <akashkrishna619@gmail.com>
2020-10-14 14:41:26 +0530

    fix: Healthcare Service Order - Source Ref Pract

--- erpnext/healthcare/doctype/patient_encounter/patient_encounter.py
+++ erpnext/healthcare/doctype/patient_encounter/patient_encounter.py
@@ -220,7 +220,9 @@ def create_healthcare_service_order(encounter):
 				'note': labtest.get_value('note'),
 				'patient_instruction': labtest.get_value('patient_instruction'),
 				'healthcare_service_unit_type':lab_template.get_value('healthcare_service_unit_type'),
-				'company':encounter.company
+				'company':encounter.company,
+				'source':encounter.source,
+				'referring_practitioner':encounter.referring_practitioner
 				}
 			make_healthcare_service_order(args)
 	if encounter.procedure_prescription:
@@ -246,8 +248,10 @@ def create_healthcare_service_order(encounter):
 				'staff_role': procedure_template.get_value('staff_role'),
 				'note': procedure.get_value('note'),
 				'patient_instruction': procedure.get_value('patient_instruction'),
-				'healthcare_service_unit_type':lab_template.get_value('healthcare_service_unit_type'),
-				'company':encounter.company
+				'healthcare_service_unit_type':procedure_template.get_value('healthcare_service_unit_type'),
+				'company':encounter.company,
+				'source':encounter.source,
+				'referring_practitioner':encounter.referring_practitioner
 				}
 			make_healthcare_service_order(args)
 	if encounter.therapies:
@@ -299,7 +303,9 @@ def create_healthcare_service_order(encounter):
 				'note': radiology.get_value('note'),
 				'patient_instruction': radiology.get_value('patient_instruction'),
 				'healthcare_service_unit_type':radiology_template.get_value('healthcare_service_unit_type'),
-				'company':encounter.company
+				'company':encounter.company,
+				'source':encounter.source,
+				'referring_practitioner':encounter.referring_practitioner
 				}
 			make_healthcare_service_order(args)
 
