8c698c8950336b2d4697040a4b208509c2aa8199
muhammad <muhammad@earthianslive.com>
2020-10-27 11:52:07 +0530

    fix:Healthcare Insurance - set sales invoice item lines

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.js
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.js
@@ -1064,6 +1064,24 @@ var get_checked_values= function($results) {
 			else{
 				checked_values['description'] = false;
 			}
+			if($(this).attr('data-discount-percentage') != 'undefined'){
+				checked_values['discount_percentage'] = $(this).attr('data-discount-percentage');
+			}
+			else{
+				checked_values['discount_percentage'] = false;
+			}
+			if($(this).attr('data-insurance-claim-coverage') != 'undefined'){
+				checked_values['insurance_claim_coverage'] = $(this).attr('data-insurance-claim-coverage');
+			}
+			else{
+				checked_values['insurance_claim_coverage'] = false;
+			}
+			if($(this).attr('data-insurance-claim') != 'undefined'){
+				checked_values['insurance_claim'] = $(this).attr('data-insurance-claim');
+			}
+			else{
+				checked_values['insurance_claim'] = false;
+			}
 			return checked_values;
 		}
 	}).get();
@@ -1080,7 +1098,7 @@ var get_drugs_to_invoice = function(frm) {
 				description:'Quantity will be calculated only for items which has "Nos" as UoM. You may change as required for each invoice item.',
 				get_query: function(doc) {
 					return {
-						filters: { 
+						filters: {
 							patient: dialog.get_value("patient"),
 							company: frm.doc.company,
 							docstatus: 1
@@ -1139,7 +1157,10 @@ var list_row_data_items = function(head, $row, result, invoice_healthcare_servic
 				data-rate = ${result.rate}
 				data-income-account = "${result.income_account}"
 				data-qty = ${result.qty}
-				data-description = "${result.description}">
+				data-description = "${result.description}"
+				data-discount-percentage = ${result.discount_percentage}
+				data-insurance-claim-coverage = ${result.insurance_claim_coverage}
+				data-insurance-claim = ${result.insurance_claim}>
 				</div>`).append($row);
 	}
 	else{
